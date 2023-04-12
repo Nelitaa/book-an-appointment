187a4231ca2876780639d2e1ecf7fec243bce27b
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-12 00:04:58 -0400

    Add create method to reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -6,15 +6,31 @@ class Api::V1::ReservationsController < ApplicationController
     render json: { reservations: @reservations }
   end
 
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: { reservation: @reservation, success: true, message: "Doctor's appointment created successfully!" }, status: :created
+    else
+      render json: { message: @reservation.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     if @reservation.destroy
-      render json: { message: 'Reservation was successfully destroyed!' }, status: :ok
+      render json: { reservation: @reservation, success: true, message: 'Reservation was successfully destroyed!' }, status: :ok
     else
       render json: { error: 'Failed to delete reservation' }, status: :unprocessable_entity
     end
   end
 
+  private
+
   def set_reservation
     @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:doctor_id, :date, :city, :user_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: "ex6@gmail.com", password: "123456")
 Doctor.create(name: "Juan Muñoz", specialization: "Pediatry", city: "Miami", fee: 200, photo: "https://st2.depositphotos.com/1743476/5738/i/950/depositphotos_57385697-stock-photo-confident-mature-doctor.jpg", experience: 20)
 Doctor.create(name: "Pedro Fuentes", specialization: "Pediatry", city: "New York", fee: 200, photo: "https://st2.depositphotos.com/1743476/5738/i/950/depositphotos_57385697-stock-photo-confident-mature-doctor.jpg", experience: 10)
 Reservation.create(user_id: 1, doctor_id: 1, date: "2023-04-10", city: "Miami")
