3bb306b927d621559ed99c9e98338f26c1c87d0d
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-11 22:38:28 -0400

    Add index to reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
   def index
-    @reservations = Reservation.all
-    render json: @reservations
+    @reservations = Reservation.includes(:doctor).all
+    render json: { reservations: @reservations }
   end
 end
