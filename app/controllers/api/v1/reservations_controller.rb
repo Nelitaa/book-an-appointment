b654e9be1d24a23d515525d70e8b2e40422b3320
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-11 23:02:08 -0400

    Create destroy method for reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[destroy]
+
   def index
     @reservations = Reservation.includes(:doctor).all
     render json: { reservations: @reservations }
+  end
+
+  def destroy
+    if @reservation.destroy
+      render json: { message: 'Reservation was successfully destroyed!' }, status: :ok
+    else
+      render json: { error: 'Failed to delete reservation' }, status: :unprocessable_entity
+    end
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
   end
 end
